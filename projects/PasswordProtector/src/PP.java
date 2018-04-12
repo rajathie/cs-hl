259138c793edef7ce7583c733e815af96b6bb34e
Markus Tiede <markus.tiede@open-co.de>
2018-04-12 17:19:02 +0200

    PasswordProtector - part 1

---
+++ projects/PasswordProtector/src/PP.java
@@ -0,0 +1,17 @@
+
+public class PP {
+
+	public static void main(String[] args) {
+		String password; 
+		String userInput;
+		boolean found = false;
+		int tries = 0;
+		
+		
+		do {
+			
+		} while ( tries < 3 && !found);
+		// de-Morgan: !(tries > 3 || found)
+	
+	}
+}
